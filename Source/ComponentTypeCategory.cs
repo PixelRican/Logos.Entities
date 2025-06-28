71cbe82401e1a12560358fa5c7b0122e845807bb
PixelRican <rimercado02@gmail.com>
2025-06-28 19:05:18 -0400

    Update documentation for ComponentTypeCategory.cs

--- Source/ComponentTypeCategory.cs
+++ Source/ComponentTypeCategory.cs
@@ -4,25 +4,25 @@
 namespace Monophyll.Entities
 {
     /// <summary>
-    /// Specifies the categories of component types.
+    /// Specifies component type categories.
     /// </summary>
     public enum ComponentTypeCategory
     {
         /// <summary>
-        /// Specifies that the component type is an unknown type.
+        /// Specifies that the component type is unknown.
         /// </summary>
-        Unknown,
+        None,
         /// <summary>
-        /// Specifies that the component type is a reference type or a value type that contains
-        /// reference type fields.
+        /// Specifies that the component type is a reference type or a value type that
+        /// contains reference type instance fields.
         /// </summary>
         Managed,
         /// <summary>
-        /// Specifies that the component type is a value type with non-reference type fields.
+        /// Specifies that the component type is a value type with unmanaged instance fields.
         /// </summary>
         Unmanaged,
         /// <summary>
-        /// Specifies that the component type is a value type with no fields.
+        /// Specifies that the component type is a value type with no instance fields.
         /// </summary>
         Tag
     }
