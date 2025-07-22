61ce8f53051847faf52e22b97da7b35a88ca22f8
PixelRican <rimercado02@gmail.com>
2025-07-22 11:35:19 -0400

    Update EntityRegistryTestFixture.cs

--- Tests/EntityRegistryTestFixture.cs
+++ Tests/EntityRegistryTestFixture.cs
@@ -6,10 +6,10 @@ using System;
 namespace Logos.Entities.Tests
 {
     [TestFixture]
-    public sealed class EntityRegistryTestFixture
+    public static class EntityRegistryTestFixture
     {
         [Test]
-        public void AddComponentTest()
+        public static void AddComponentTest()
         {
             EntityRegistry registry = new EntityRegistry();
             ReadOnlySpan<ComponentType> componentTypes =
@@ -25,19 +25,27 @@ namespace Logos.Entities.Tests
             ];
             Entity entity = registry.CreateEntity();
 
-            Assert.Throws<ArgumentException>(
-                () => registry.AddComponent(new Entity(-1, -1), null!));
-
-            foreach (ComponentType componentType in componentTypes)
+            Assert.Throws<ArgumentException>(() =>
             {
-                Assert.That(registry.AddComponent(entity, componentType));
-                Assert.That(registry.HasComponent(entity, componentType));
-                //Assert.That(componentTypes.StartsWith(registry.FindEntity(entity, out _).Archetype.ComponentTypes));
+                registry.AddComponent(new Entity(-1, -1), null!);
+            });
+
+            for (int i = 0; i < componentTypes.Length; i++)
+            {
+                ComponentType componentType = componentTypes[i];
+
+                using (Assert.EnterMultipleScope())
+                {
+                    Assert.That(registry.AddComponent(entity, componentType), Is.True);
+                    Assert.That(registry.HasComponent(entity, componentType), Is.True);
+                    Assert.That(registry.FindEntity(entity, out _).Archetype.ComponentTypes
+                        .SequenceEqual(componentTypes.Slice(0, i + 1)), Is.True);
+                }
             }
         }
 
         [Test]
-        public void CreateEntityTest()
+        public static void CreateEntityTest()
         {
             EntityRegistry registry = new EntityRegistry();
 
@@ -45,13 +53,16 @@ namespace Logos.Entities.Tests
             {
                 Entity entity = registry.CreateEntity();
 
-                Assert.That(new Entity(i, 0), Is.EqualTo(entity));
-                Assert.That(registry.ContainsEntity(entity));
-                Assert.That(registry.FindEntity(entity, out _).Archetype, Is.SameAs(EntityArchetype.Base));
-                Assert.That(registry.FindEntity(entity, out _).GetEntities()[i], Is.EqualTo(entity));
+                using (Assert.EnterMultipleScope())
+                {
+                    Assert.That(new Entity(i, 0), Is.EqualTo(entity));
+                    Assert.That(registry.ContainsEntity(entity));
+                    Assert.That(registry.FindEntity(entity, out _).Archetype, Is.SameAs(EntityArchetype.Base));
+                    Assert.That(registry.FindEntity(entity, out _).GetEntities()[i], Is.EqualTo(entity));
+                }
             }
 
-            Assert.That(10, Is.EqualTo(registry.Count));
+            Assert.That(registry.Count, Is.EqualTo(10));
 
             EntityArchetype archetype = registry.CreateArchetype([ComponentType.TypeOf<Name>()]);
 
@@ -59,17 +70,20 @@ namespace Logos.Entities.Tests
             {
                 Entity entity = registry.CreateEntity(archetype);
 
-                Assert.That(new Entity(i + 10, 0), Is.EqualTo(entity));
-                Assert.That(registry.HasComponent(entity, ComponentType.TypeOf<Name>()));
-                Assert.That(registry.FindEntity(entity, out _).Archetype, Is.SameAs(archetype));
-                Assert.That(registry.FindEntity(entity, out _).GetEntities()[i], Is.EqualTo(entity));
+                using (Assert.EnterMultipleScope())
+                {
+                    Assert.That(new Entity(i + 10, 0), Is.EqualTo(entity));
+                    Assert.That(registry.HasComponent(entity, ComponentType.TypeOf<Name>()));
+                    Assert.That(registry.FindEntity(entity, out _).Archetype, Is.SameAs(archetype));
+                    Assert.That(registry.FindEntity(entity, out _).GetEntities()[i], Is.EqualTo(entity));
+                }
             }
 
-            Assert.That(20, Is.EqualTo(registry.Count));
+            Assert.That(registry.Count, Is.EqualTo(20));
         }
 
         [Test]
-        public void DestroyEntityTest()
+        public static void DestroyEntityTest()
         {
             EntityRegistry registry = new EntityRegistry();
 
@@ -85,11 +99,15 @@ namespace Logos.Entities.Tests
             for (int i = 0; i < count; i++)
             {
                 Entity entity = entities[i];
-                Assert.That(registry.DestroyEntity(entity));
-                Assert.That(registry.ContainsEntity(entity), Is.False);
+
+                using (Assert.EnterMultipleScope())
+                {
+                    Assert.That(registry.DestroyEntity(entity));
+                    Assert.That(registry.ContainsEntity(entity), Is.False);
+                }
             }
 
-            Assert.That(0, Is.EqualTo(registry.Count));
+            Assert.That(registry.Count, Is.Zero);
 
             for (int i = 9; i >= 0; i--)
             {
@@ -98,7 +116,7 @@ namespace Logos.Entities.Tests
         }
 
         [Test]
-        public void RemoveComponentTest()
+        public static void RemoveComponentTest()
         {
             EntityRegistry registry = new EntityRegistry();
             ReadOnlySpan<ComponentType> componentTypes =
@@ -114,14 +132,22 @@ namespace Logos.Entities.Tests
             ];
             Entity entity = registry.CreateEntity(componentTypes);
 
-            Assert.Throws<ArgumentException>(
-                () => registry.RemoveComponent(new Entity(-1, -1), null!));
-
-            foreach (ComponentType componentType in componentTypes)
+            Assert.Throws<ArgumentException>(() =>
             {
-                Assert.That(registry.RemoveComponent(entity, componentType));
-                Assert.That(registry.HasComponent(entity, componentType), Is.False);
-                //Assert.That(componentTypes.EndsWith(registry.FindEntity(entity, out _).Archetype.ComponentTypes));
+                registry.RemoveComponent(new Entity(-1, -1), null!);
+            });
+
+            for (int i = 0; i < componentTypes.Length; i++)
+            {
+                ComponentType componentType = componentTypes[i];
+
+                using (Assert.EnterMultipleScope())
+                {
+                    Assert.That(registry.RemoveComponent(entity, componentType));
+                    Assert.That(registry.HasComponent(entity, componentType), Is.False);
+                    Assert.That(registry.FindEntity(entity, out _).Archetype.ComponentTypes
+                        .SequenceEqual(componentTypes.Slice(i + 1)), Is.True);
+                }
             }
         }
     }
