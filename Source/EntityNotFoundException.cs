f05252d540c19cdf85a0e0917256a910fcda2efc
PixelRican <rimercado02@gmail.com>
2025-08-11 13:00:50 -0400

    Update documentation for EntityNotFoundException class

--- Source/EntityNotFoundException.cs
+++ Source/EntityNotFoundException.cs
@@ -6,8 +6,8 @@ using System;
 namespace Logos.Entities
 {
     /// <summary>
-    /// The exception that is thrown when an entity could not be found within an
-    /// <see cref="EntityRegistry"/>.
+    /// The exception that is thrown when an attempt to access a entity that does not exist in a
+    /// data structure fails.
     /// </summary>
     public class EntityNotFoundException : Exception
     {
@@ -16,7 +16,7 @@ namespace Logos.Entities
         /// default error message.
         /// </summary>
         public EntityNotFoundException()
-            : base("The specified entity could not be found.")
+            : base("Unable to find the specified entity.")
         {
         }
 
@@ -24,7 +24,6 @@ namespace Logos.Entities
         /// Initializes a new instance of the <see cref="EntityNotFoundException"/> class with the
         /// specified error message.
         /// </summary>
-        /// 
         /// <param name="message">
         /// The message that describes the error.
         /// </param>
@@ -35,14 +34,12 @@ namespace Logos.Entities
 
         /// <summary>
         /// Initializes a new instance of the <see cref="EntityNotFoundException"/> class with the
-        /// specified error message and a reference to the inner exception that is the cause of
-        /// this exception.
+        /// specified error message and a reference to the inner exception that is the cause of this
+        /// exception.
         /// </summary>
-        /// 
         /// <param name="message">
         /// The message that describes the error.
         /// </param>
-        /// 
         /// <param name="innerException">
         /// The exception that is the cause of the <see cref="EntityNotFoundException"/>, or
         /// <see langword="null"/> if no inner exception is specified.
