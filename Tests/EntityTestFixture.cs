5f14388c34b259c74f565585ba0c106d5a0760d2
PixelRican <rimercado02@gmail.com>
2025-08-19 07:04:24 -0400

    Update Entity unit test

--- Tests/EntityTestFixture.cs
+++ Tests/EntityTestFixture.cs
@@ -1,24 +1,65 @@
 ﻿// Copyright (c) 2025 Roberto I. Mercado
 // Released under the MIT License. See LICENSE for details.
 
-using System.Collections.Generic;
+using System.Collections;
 
 namespace Logos.Entities.Tests
 {
     [TestFixture]
     public static class EntityTestFixture
     {
-        [TestCaseSource(typeof(EntityTestCaseSource), nameof(EntityTestCaseSource.EqualsTestCases))]
-        public static void EqualsTest(Entity source, Entity other)
+        private static IEnumerable EqualsTestCases
         {
-            EqualityComparer<Entity> comparer = EqualityComparer<Entity>.Default;
+            get
+            {
+                // Compare entities with same indices and versions.
+                yield return new object[]
+                {
+                    new Entity(0, 0),
+                    new Entity(0, 0),
+                    true
+                };
 
+                // Compare entities with different indices and versions.
+                yield return new object[]
+                {
+                    new Entity(0, 0),
+                    new Entity(1, 1),
+                    false
+                };
+
+                // Compare entities with same indices and different versions.
+                yield return new object[]
+                {
+                    new Entity(0, 0),
+                    new Entity(0, 1),
+                    false
+                };
+
+                // Compare entities with different indices and same versions.
+                yield return new object[]
+                {
+                    new Entity(0, 0),
+                    new Entity(1, 0),
+                    false
+                };
+            }
+        }
+
+        [TestCaseSource(nameof(EqualsTestCases))]
+        public static void EqualsTest(Entity left, Entity right, bool expected)
+        {
             using (Assert.EnterMultipleScope())
             {
-                Assert.That(comparer.Equals(source, source), Is.True);
-                Assert.That(comparer.Equals(other, other), Is.True);
-                Assert.That(comparer.Equals(source, other), Is.False);
-                Assert.That(comparer.Equals(other, source), Is.False);
+                Assert.That(left.Equals(right), Is.EqualTo(expected));
+                Assert.That(left.Equals(right as object), Is.EqualTo(expected));
+                Assert.That(left == right, Is.EqualTo(expected));
+                Assert.That(left != right, Is.Not.EqualTo(expected));
+
+                if (expected)
+                {
+                    Assert.That(left.GetHashCode(), Is.EqualTo(right.GetHashCode()));
+                }
             }
         }
     }
